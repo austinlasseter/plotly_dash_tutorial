16a356ae9b638ea32d81db98e127e89807d9b3fb
Lasseter <alasseter@deloitte.com>
2018-07-16 13:55:04 -0400

    added titanic dataset

--- callback1.py
+++ callback1.py
@@ -1,15 +1,23 @@
+'''
+A simple callback example
+'''
+
+# imports
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
+# instantiate
 app = dash.Dash()
 
+# layout
 app.layout = html.Div([
     dcc.Input(id='my-id', value='initial value', type='text'),
     html.Div(id='my-div')
 ])
 
+# callback decorators and functions
 @app.callback(
     Output(component_id='my-div', component_property='children'),
     [Input(component_id='my-id', component_property='value')]
@@ -17,5 +25,6 @@ app.layout = html.Div([
 def update_output_div(input_value):
     return 'You\'ve entered "{}"'.format(input_value)
 
+# execute
 if __name__ == '__main__':
     app.run_server()
